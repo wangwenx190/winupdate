0b1272120ffe7dce8a445909f4fcd79ad36b20dd
Yuhang Zhao <2546789017@qq.com>
2022-05-27 15:07:06 +0800

    wip
    Signed-off-by: Yuhang Zhao <2546789017@qq.com>

--- main.cpp
+++ main.cpp
@@ -134,14 +134,14 @@ static inline void RestartAsElevatedProcess()
 
 static inline void EnableMicrosoftUpdate()
 {
-    CComPtr<IUpdateServiceManager2> pUsm = nullptr;
-    HRESULT hr = CoCreateInstance(CLSID_UpdateServiceManager, nullptr, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&pUsm));
+    CComPtr<IUpdateServiceManager2> pUpdateServiceManager = nullptr;
+    HRESULT hr = CoCreateInstance(CLSID_UpdateServiceManager, nullptr, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&pUpdateServiceManager));
     if (FAILED(hr)) {
         std::wcerr << VT_RED << L"CoCreateInstance() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
         return;
     }
     const BSTR appId = SysAllocString(L"My App");
-    hr = pUsm->put_ClientApplicationID(appId);
+    hr = pUpdateServiceManager->put_ClientApplicationID(appId);
     if (FAILED(hr)) {
         SysFreeString(appId);
         std::wcerr << VT_RED << L"put_ClientApplicationID() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
@@ -150,8 +150,8 @@ static inline void EnableMicrosoftUpdate()
     const BSTR serviceId = SysAllocString(L"7971f918-a847-4430-9279-4a52d1efe18d");
     const BSTR authorizationCabPath = SysAllocString(L"");
     const DWORD flags = (AddServiceFlag::asfAllowPendingRegistration | AddServiceFlag::asfAllowOnlineRegistration | AddServiceFlag::asfRegisterServiceWithAU);
-    CComPtr<IUpdateServiceRegistration> pUsr = nullptr;
-    hr = pUsm->AddService2(serviceId, flags, authorizationCabPath, &pUsr);
+    CComPtr<IUpdateServiceRegistration> pUpdateServiceRegistration = nullptr;
+    hr = pUpdateServiceManager->AddService2(serviceId, flags, authorizationCabPath, &pUpdateServiceRegistration);
     if (FAILED(hr)) {
         std::wcerr << VT_RED << L"AddService2() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
     }
@@ -229,63 +229,255 @@ static inline void UpdateSystem()
 {
     std::wcout << VT_CYAN << L"Start updating system ..." << VT_DEFAULT << std::endl;
 
-    CComPtr<IAutomaticUpdates2> pAu = nullptr;
-    HRESULT hr = CoCreateInstance(CLSID_AutomaticUpdates, nullptr, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&pAu));
-    if (FAILED(hr)) {
-        std::wcerr << VT_RED << L"CoCreateInstance() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
-        return;
-    }
-    hr = pAu->EnableService();
-    if (FAILED(hr)) {
-        std::wcerr << VT_RED << L"EnableService() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
-        //return;
-    }
-    CComPtr<IAutomaticUpdatesSettings3> pAus = nullptr;
-    hr = pAu->get_Settings(reinterpret_cast<IAutomaticUpdatesSettings **>(&pAus));
-    if (FAILED(hr)) {
-        std::wcerr << VT_RED << L"get_Settings() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
-        return;
-    }
-    hr = pAus->Refresh();
-    if (FAILED(hr)) {
-        std::wcerr << VT_RED << L"Refresh() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
-        return;
-    }
-    hr = pAus->put_NotificationLevel(aunlScheduledInstallation);
-    if (FAILED(hr)) {
-        std::wcerr << VT_RED << L"put_NotificationLevel() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
-        return;
-    }
-    hr = pAus->put_IncludeRecommendedUpdates(VARIANT_TRUE);
-    if (FAILED(hr)) {
-        std::wcerr << VT_RED << L"put_IncludeRecommendedUpdates() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
-        return;
-    }
-    hr = pAus->put_FeaturedUpdatesEnabled(VARIANT_TRUE);
-    if (FAILED(hr)) {
-        std::wcerr << VT_RED << L"put_FeaturedUpdatesEnabled() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
-        return;
-    }
-    hr = pAus->put_NonAdministratorsElevated(VARIANT_TRUE);
-    if (FAILED(hr)) {
-        std::wcerr << VT_RED << L"put_NonAdministratorsElevated() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
-        return;
-    }
-    hr = pAus->Save();
-    if (FAILED(hr)) {
-        std::wcerr << VT_RED << L"Save() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
-        return;
-    }
-    hr = pAu->DetectNow();
-    if (FAILED(hr)) {
-        std::wcerr << VT_RED << L"DetectNow() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
-        return;
-    }
-    CComPtr<IAutomaticUpdatesResults> pAur = nullptr;
-    hr = pAu->get_Results(&pAur);
-    if (FAILED(hr)) {
-        std::wcerr << VT_RED << L"get_Results() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
-        return;
+    while (true) {
+#if 0
+        CComPtr<IAutomaticUpdates2> pAutomaticUpdates = nullptr;
+        HRESULT hr = CoCreateInstance(CLSID_AutomaticUpdates, nullptr, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&pAutomaticUpdates));
+        if (FAILED(hr)) {
+            std::wcerr << VT_RED << L"CoCreateInstance() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        hr = pAutomaticUpdates->EnableService();
+        if (FAILED(hr)) {
+            std::wcerr << VT_RED << L"EnableService() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            //return;
+        }
+        CComPtr<IAutomaticUpdatesSettings3> pAutomaticUpdatesSettings = nullptr;
+        hr = pAutomaticUpdates->get_Settings(reinterpret_cast<IAutomaticUpdatesSettings **>(&pAutomaticUpdatesSettings));
+        if (FAILED(hr)) {
+            std::wcerr << VT_RED << L"get_Settings() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        hr = pAutomaticUpdatesSettings->Refresh();
+        if (FAILED(hr)) {
+            std::wcerr << VT_RED << L"Refresh() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        hr = pAutomaticUpdatesSettings->put_NotificationLevel(aunlScheduledInstallation);
+        if (FAILED(hr)) {
+            std::wcerr << VT_RED << L"put_NotificationLevel() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        hr = pAutomaticUpdatesSettings->put_IncludeRecommendedUpdates(VARIANT_TRUE);
+        if (FAILED(hr)) {
+            std::wcerr << VT_RED << L"put_IncludeRecommendedUpdates() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        hr = pAutomaticUpdatesSettings->put_FeaturedUpdatesEnabled(VARIANT_TRUE);
+        if (FAILED(hr)) {
+            std::wcerr << VT_RED << L"put_FeaturedUpdatesEnabled() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        hr = pAutomaticUpdatesSettings->put_NonAdministratorsElevated(VARIANT_TRUE);
+        if (FAILED(hr)) {
+            std::wcerr << VT_RED << L"put_NonAdministratorsElevated() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        hr = pAutomaticUpdatesSettings->Save();
+        if (FAILED(hr)) {
+            std::wcerr << VT_RED << L"Save() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        hr = pAutomaticUpdates->DetectNow();
+        if (FAILED(hr)) {
+            std::wcerr << VT_RED << L"DetectNow() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        CComPtr<IAutomaticUpdatesResults> pAutomaticUpdatesResults = nullptr;
+        hr = pAutomaticUpdates->get_Results(&pAutomaticUpdatesResults);
+        if (FAILED(hr)) {
+            std::wcerr << VT_RED << L"get_Results() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        break;
+#else
+        CComPtr<IUpdateSession3> pUpdateSession = nullptr;
+        HRESULT hr = CoCreateInstance(CLSID_UpdateSession, nullptr, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&pUpdateSession));
+        if (FAILED(hr)) {
+            std::wcerr << VT_RED << L"CoCreateInstance() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        const BSTR appId = SysAllocString(L"My App");
+        hr = pUpdateSession->put_ClientApplicationID(appId);
+        if (FAILED(hr)) {
+            SysFreeString(appId);
+            std::wcerr << VT_RED << L"put_ClientApplicationID() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        CComPtr<IUpdateSearcher3> pUpdateSearcher = nullptr;
+        hr = pUpdateSession->CreateUpdateSearcher(reinterpret_cast<IUpdateSearcher **>(&pUpdateSearcher));
+        if (FAILED(hr)) {
+            SysFreeString(appId);
+            std::wcerr << VT_RED << L"CreateUpdateSearcher() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        hr = pUpdateSearcher->put_CanAutomaticallyUpgradeService(VARIANT_TRUE);
+        if (FAILED(hr)) {
+            //SysFreeString(appId);
+            std::wcerr << VT_RED << L"put_CanAutomaticallyUpgradeService() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            //return;
+        }
+        hr = pUpdateSearcher->put_Online(VARIANT_TRUE);
+        if (FAILED(hr)) {
+            SysFreeString(appId);
+            std::wcerr << VT_RED << L"put_Online() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        hr = pUpdateSearcher->put_ServerSelection(ssWindowsUpdate);
+        if (FAILED(hr)) {
+            SysFreeString(appId);
+            std::wcerr << VT_RED << L"put_ServerSelection() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        hr = pUpdateSearcher->put_IncludePotentiallySupersededUpdates(VARIANT_FALSE);
+        if (FAILED(hr)) {
+            SysFreeString(appId);
+            std::wcerr << VT_RED << L"put_IncludePotentiallySupersededUpdates() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        const BSTR criteria = SysAllocString(L"( IsInstalled = 0 AND IsHidden = 0 )");
+        CComPtr<ISearchResult> pSearchResult = nullptr;
+        hr = pUpdateSearcher->Search(criteria, &pSearchResult);
+        if (FAILED(hr)) {
+            SysFreeString(criteria);
+            SysFreeString(appId);
+            std::wcerr << VT_RED << L"Search() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        OperationResultCode searchResultCode = orcNotStarted;
+        hr = pSearchResult->get_ResultCode(&searchResultCode);
+        if (FAILED(hr)) {
+            SysFreeString(criteria);
+            SysFreeString(appId);
+            std::wcerr << VT_RED << L"get_ResultCode() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        if (searchResultCode != orcSucceeded) {
+            SysFreeString(criteria);
+            SysFreeString(appId);
+            std::wcerr << VT_RED << L"Failed to search for Windows updates." << VT_DEFAULT << std::endl;
+            return;
+        }
+        CComPtr<IUpdateCollection> pUpdateCollection = nullptr;
+        hr = pSearchResult->get_Updates(&pUpdateCollection);
+        if (FAILED(hr)) {
+            SysFreeString(criteria);
+            SysFreeString(appId);
+            std::wcerr << VT_RED << L"get_Updates() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        LONG updateCount = 0;
+        hr = pUpdateCollection->get_Count(&updateCount);
+        if (FAILED(hr)) {
+            SysFreeString(criteria);
+            SysFreeString(appId);
+            std::wcerr << VT_RED << L"get_Count() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        if (updateCount < 1) {
+            SysFreeString(criteria);
+            SysFreeString(appId);
+            break;
+        }
+        std::wcout << L"Found " << updateCount << L" updates in total." << std::endl;
+        CComPtr<IUpdateDownloader> pUpdateDownloader = nullptr;
+        hr = pUpdateSession->CreateUpdateDownloader(&pUpdateDownloader);
+        if (FAILED(hr)) {
+            SysFreeString(criteria);
+            SysFreeString(appId);
+            std::wcerr << VT_RED << L"CreateUpdateDownloader() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        hr = pUpdateDownloader->put_Updates(pUpdateCollection);
+        if (FAILED(hr)) {
+            SysFreeString(criteria);
+            SysFreeString(appId);
+            std::wcerr << VT_RED << L"put_Updates() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        CComPtr<IDownloadResult> pDownloadResult = nullptr;
+        hr = pUpdateDownloader->Download(&pDownloadResult);
+        if (FAILED(hr)) {
+            SysFreeString(criteria);
+            SysFreeString(appId);
+            std::wcerr << VT_RED << L"Download() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        OperationResultCode downloadResultCode = orcNotStarted;
+        hr = pDownloadResult->get_ResultCode(&downloadResultCode);
+        if (FAILED(hr)) {
+            SysFreeString(criteria);
+            SysFreeString(appId);
+            std::wcerr << VT_RED << L"get_ResultCode() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        if (downloadResultCode != orcSucceeded) {
+            SysFreeString(criteria);
+            SysFreeString(appId);
+            std::wcerr << VT_RED << L"Failed to download Windows updates." << VT_DEFAULT << std::endl;
+            return;
+        }
+        CComPtr<IUpdateInstaller4> pUpdateInstaller = nullptr;
+        hr = pUpdateSession->CreateUpdateInstaller(reinterpret_cast<IUpdateInstaller **>(&pUpdateInstaller));
+        if (FAILED(hr)) {
+            SysFreeString(criteria);
+            SysFreeString(appId);
+            std::wcerr << VT_RED << L"CreateUpdateInstaller() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        hr = pUpdateInstaller->put_Updates(pUpdateCollection);
+        if (FAILED(hr)) {
+            SysFreeString(criteria);
+            SysFreeString(appId);
+            std::wcerr << VT_RED << L"put_Updates() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        hr = pUpdateInstaller->put_ForceQuiet(VARIANT_TRUE);
+        if (FAILED(hr)) {
+            SysFreeString(criteria);
+            SysFreeString(appId);
+            std::wcerr << VT_RED << L"put_ForceQuiet() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        hr = pUpdateInstaller->put_AttemptCloseAppsIfNecessary(VARIANT_TRUE);
+        if (FAILED(hr)) {
+            SysFreeString(criteria);
+            SysFreeString(appId);
+            std::wcerr << VT_RED << L"put_AttemptCloseAppsIfNecessary() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        CComPtr<IInstallationResult> pInstallationResult = nullptr;
+        hr = pUpdateInstaller->Install(&pInstallationResult);
+        if (FAILED(hr)) {
+            SysFreeString(criteria);
+            SysFreeString(appId);
+            std::wcerr << VT_RED << L"Install() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        OperationResultCode installationResultCode = orcNotStarted;
+        hr = pInstallationResult->get_ResultCode(&installationResultCode);
+        if (FAILED(hr)) {
+            SysFreeString(criteria);
+            SysFreeString(appId);
+            std::wcerr << VT_RED << L"get_ResultCode() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        if (installationResultCode != orcSucceeded) {
+            SysFreeString(criteria);
+            SysFreeString(appId);
+            std::wcerr << VT_RED << L"Failed to install Windows updates." << VT_DEFAULT << std::endl;
+            return;
+        }
+        hr = pUpdateInstaller->Commit(0);
+        if (FAILED(hr)) {
+            SysFreeString(criteria);
+            SysFreeString(appId);
+            std::wcerr << VT_RED << L"Commit() failed with error " << GetSystemErrorMessage(HRESULT_CODE(hr)) << VT_DEFAULT << std::endl;
+            return;
+        }
+        SysFreeString(criteria);
+        SysFreeString(appId);
+#endif
     }
 
     std::wcout << VT_GREEN << L"Congratulations! Your system is update to date!" << VT_DEFAULT << std::endl;
